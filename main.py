e09850b262fb1d07d91aa590fbf32df82ebd2f87
David <36579697+lidj22@users.noreply.github.com>
2023-11-22 17:24:35 -0800

    add/delete certificates

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ logger.info("Starting script.")
 subprocess.call("./scripts/generate.sh", stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 logger.info("Passed credential generation stage.")
 
+# add CA (mac)
+subprocess.run(["security", "add-trusted-cert", "-d", "-r", "trustRoot", "-k", "/Library/Keychains/System.keychain", "./out/CA.pem"])
+logger.info("Added test certificate authority.")
+
 subprocess.run(["docker", "build", "-t", "cert-test", "."], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 logger.info("Passed container build stage.")
 
@@ -16,6 +20,10 @@ subprocess.run(["docker", "run", "-d", "-p", "443:443", "--name", "cert-test", "
 logger.info("Passed container run stage.")
 
 subprocess.run(["curl", "https://localhost:443"])
+
+# remove CA (mac)
+subprocess.run(["security", "delete-certificate", "-c", "special-name", "-t"])
+logger.info("Deleted test certificate authority.")
 
 subprocess.run(["docker", "stop", "cert-test"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 logger.info("Stopped container.")
